4427e46ae6fb8e45a8bd20cd4baa804e8bcfc2e8
Ja <>
2018-03-09 13:29:02 +0100

    Interfejs

--- Zadania_probne/src/Zadania.java
+++ Zadania_probne/src/Zadania.java
@@ -1,19 +1,38 @@
 
 import java.util.Scanner;
 
-
 public class Zadania {
 
-    
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Podaj nr zadania: ");
+        System.out.println("Podaj nr zadania: (od 1 do 7)(0 aby wyjść)");
         int n = input.nextInt();
-        
-        MojeZadania z1 = new MojeZadania(n);
-        
-        
+
+        MojeZadania z1 = new MojeZadania();
+
+        int a = 0;
+
+        while (a == 0) {
+            if (n == 1) {
+                z1.zadanie1();
+            } else if (n == 2) {
+                z1.zadanie2();
+            } else if (n == 3) {
+                z1.zadanie3();
+            } else if (n == 4) {
+                z1.zadanie4();
+            } else if (n == 5) {
+                z1.zadanie5();
+            } else if (n == 6) {
+                z1.zadanie6();
+            } else if (n == 7) {
+                z1.zadanie7();
+            } else if (n == 0) {
+                a = 1;
+            } else {
+                System.out.println("Zły wybór !!!");
+            }
+        }
+
     }
-    
-}
