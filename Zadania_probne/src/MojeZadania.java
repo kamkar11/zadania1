957c8be6c27897671754ae3b0d73974a08d596d1
Ja <>
2018-03-09 13:06:46 +0100

    metody

--- Zadania_probne/src/MojeZadania.java
+++ Zadania_probne/src/MojeZadania.java
@@ -7,6 +7,36 @@ public class MojeZadania {
         this.nrZadania = nrZadania;
     }
     
+    public void zadanie1()
+    {
+        
+    }
+    
+    public void zadanie2()
+    {
+        
+    }
+    
+    public void zadanie3()
+    {
+        
+    }
+    public void zadanie4()
+    {
+        
+    }
+    public void zadanie5()
+    {
+        
+    }
+    public void zadanie6()
+    {
+        
+    }
+    public void zadanie7()
+    {
+        
+    }
     
     
     
