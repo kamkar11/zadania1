9f6f02392532f2f782b5561afafff66ed83022f0
Ja <>
2018-03-09 13:13:49 +0100

    Konstruktor

--- Zadania_probne/src/Zadania.java
+++ Zadania_probne/src/Zadania.java
@@ -1,20 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author XYZ
- */
+import java.util.Scanner;
+
+
 public class Zadania {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj nr zadania: ");
+        int n = input.nextInt();
+        
+        MojeZadania z1 = new MojeZadania(n);
+        
+        
     }
     
 }
